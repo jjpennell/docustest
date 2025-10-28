1a89b999682baaa5685e77769dd1efdf3287c8d4
Joe Pennell <binary@nycap.rr.com>
2025-10-28 15:37:56 -0400

    .docusaurus config files

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("E:\\docs\\my-website\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
+  require("E:\\docs\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("E:\\docs\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("E:\\docs\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("E:\\docs\\my-website\\src\\css\\custom.css"),
+];
